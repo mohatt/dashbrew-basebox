0bcd31c34d565dd5fc7f17be5048f2bd08dd2b3f
Mohamed Elkholy <mkh117@gmail.com>
2014-09-11 03:40:32 +0300

    Removed all versions of PHP and kept only the 5.6.0

--- scripts/user.sh
+++ scripts/user.sh
@@ -6,25 +6,12 @@ export PHPBREW_ROOT=/opt/phpbrew
 /usr/bin/phpbrew init
 source $PHPBREW_ROOT/bashrc
 
-# install phps
-/usr/bin/phpbrew install 5.3.29 +dev
-/usr/bin/phpbrew install 5.4.32 +dev
-/usr/bin/phpbrew install 5.5.16 +dev
+# install php 5.6
 /usr/bin/phpbrew install 5.6.0 +dev
 
 # install php extensions
-phpbrew use 5.3.29
+phpbrew switch 5.6.0
 /usr/bin/phpbrew ext install +dev
-phpbrew use 5.4.32
-/usr/bin/phpbrew ext install +dev
-phpbrew use 5.5.16
-/usr/bin/phpbrew ext install +dev
-phpbrew use 5.6.0
-/usr/bin/phpbrew ext install +dev
-
-# install php tools
-phpbrew install-composer
-phpbrew install-phpunit
 
 # oh my zsh
 git clone -b phpbrew https://github.com/mdkholy/oh-my-zsh.git .oh-my-zsh

d4249542200d91b2467b3f9388998f37a1b7ac25
Mohamed Elkholy <mkh117@gmail.com>
2015-03-02 09:45:41 +0200

    Added update patch 0.1.2

---
+++ patches/0.1.2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# fixes github.com/phpbrew/phpbrew/issues/433 by adding --with-icu-dir=/usr
+function update_phpbrew_config() {
+    local phpbrew_config="variants:
+    dev:
+        bcmath:
+        bz2:
+        calendar:
+        cli:
+        fpm:
+        phpdbg:
+        curl:
+        ctype:
+        xml:
+        dom:
+        filter:
+        ipc:
+        json:
+        mbregex:
+        mbstring:
+        hash:
+        mcrypt:
+        readline:
+        posix:
+        pcntl:
+        sockets:
+        exif:
+        gd:
+        phar:
+        pdo:
+        mysql:
+            - --with-mysql=mysqlnd
+            - --with-mysqli=mysqlnd
+            - --with-pdo-mysql=mysqlnd
+            - --with-mysql-sock=/var/run/mysqld/mysqld.sock
+        pgsql:
+        sqlite:
+        ftp:
+        kerberos:
+        iconv:
+        icu:
+            - --with-icu-dir=/usr
+        intl:
+        gettext:
+        session:
+        zip:
+        zlib:
+        openssl:
+        soap:
+        tokenizer:
+        xmlrpc:
+        tidy:
+extensions:
+    dev:
+        xhprof: latest
+        xdebug: stable
+"
+echo "$phpbrew_config" > /opt/phpbrew/config.yaml
+}
+
+update_phpbrew_config
+
+exit 0
